32c00248501c7930c4baf9f8575fddf84086b8da
Carlos Eduardo <carloseduardokf@gmail.com>
2024-10-21 15:37:14 -0300

    Adicionando pasta de exercicios 09

---
+++ curso-de-js-roger-melo/exercicio-09/app.ts
@@ -0,0 +1,10 @@
+
+// const f = (callback: (a: number) => {}) => {
+//     callback(11)
+//   }
+//   const resultado = (valor: number) => {
+//     return valor * 3 
+//   }
+  
+//   resultado('')
+
